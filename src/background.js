4a6b424212d018b5a74f8c58bdca54680c7b7d9c
Tom Headifen <thomas@headifen.com>
2017-07-29 14:37:12 -0400

    content script renamed and popup is now greyed out when not on correct page

---
+++ src/background.js
@@ -0,0 +1,23 @@
+/**
+ * Activate the extension only on certain pages
+**/
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'https://www.youtube.com/watch?v=' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
